29b8874266b46a9886438530b8eae4167aa994bb
Gurankit Singh Behal <gurankitbehal78114@gmail.com>
2022-12-10 00:26:21 +0530

    first commit

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const mongoose=require('mongoose');
 const dotenv=require ('dotenv');
 const express=require('express');
+const cors=require('cors');
 const app=express();
 
 dotenv.config({path:'./config.env'});
 require('./db/conn')
+app.use(cors());
 app.use(express.json());
 app.use(require('./router/auth'));
 const PORT=process.env.PORT;
